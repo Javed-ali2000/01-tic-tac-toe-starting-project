737c6895edfab988506c69510c65e3cc8eef35f4
Javed Ali <javedalfaaz94@gmail.com>
2024-01-21 17:56:56 +0530

    MadePlayer div succesfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
-  
-
   return (
-   <div></div>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName={"Player 1"} symbol={"X"} />
+          <Player initialName={"Player 2"} symbol={"O"} />
+        </ol>
+        <GameBoard />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
